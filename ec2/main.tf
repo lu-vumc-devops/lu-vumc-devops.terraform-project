e9debcf24f9bc27a4460a1df023ee1c9d8421f98
Sergey Pokatov <sergey@pokatov.dev>
2022-04-26 12:18:16 +0300

    Testing infracost action

--- ec2/main.tf
+++ ec2/main.tf
@@ -37,6 +37,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+# This is a comment
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
