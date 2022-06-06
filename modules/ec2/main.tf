2a1b22e15d65f95f650b4c57fd2a7a6bc01b41b6
Sergey Pokatov <sergey@pokatov.dev>
2022-06-06 12:13:35 +0300

    Added instance_profile to EC2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,10 +4,40 @@ resource "aws_instance" "web" {
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.allow_http_https.id]
   associate_public_ip_address = true
-
-  user_data = var.user_data
+  iam_instance_profile        = aws_iam_instance_profile.ec2_iam_role_instance_profile.id
+  user_data                   = var.user_data
 
   tags = var.tags
+}
+
+resource "aws_iam_role" "ec2_iam_role" {
+  name = "ec2_iam_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_iam_role_policy_attachment" {
+  role       = aws_iam_role.ec2_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_iam_role_instance_profile" {
+  name = "ec2_iam_role_instance_profile"
+  role = aws_iam_role.ec2_iam_role.name
 }
 
 resource "aws_security_group" "allow_http_https" {
